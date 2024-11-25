dd8cd43f15a6470b564e80074c717b31e853ec49
Nikodem Keller <niko.zawodowiec@gmail.com>
2024-11-25 03:32:32 +0100

    Lab 6 fix

--- src/main/java/org/example/player/model/dto/function/RequestToClubFunction.java
+++ src/main/java/org/example/player/model/dto/function/RequestToClubFunction.java
@@ -3,13 +3,15 @@ package org.example.player.model.dto.function;
 import org.example.player.entity.Club;
 import org.example.player.model.dto.PutClubRequest;
 
+import java.util.UUID;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
-public class RequestToClubFunction implements Function<PutClubRequest, Club>{
+public class RequestToClubFunction implements BiFunction<UUID,PutClubRequest, Club> {
     @Override
-    public Club apply(PutClubRequest putClubRequest) {
+    public Club apply(UUID id, PutClubRequest putClubRequest) {
         return Club.builder()
-                .id(putClubRequest.getId())
+                .id(id)
                 .name(putClubRequest.getName())
                 .budget(putClubRequest.getBudget())
                 .description(putClubRequest.getDescription())
